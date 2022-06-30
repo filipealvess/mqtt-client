77afd72b3665ba365fffad2ad26b20742e801fa0
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-06-30 20:04:20 -0300

    Update subscriber to receive payloads and publish in a topic

--- sub.py
+++ sub.py
@@ -1,22 +1,33 @@
 # Subscriber
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 
 host = 'test.mosquitto.org'
 port = 1883
 
 def on_message(client, userdata, msg):
-  print(msg.topic + " = " + str(msg.payload))
+  topic = msg.topic
+  payload = float(msg.payload.decode())
+  high_temperature = (topic == 'temperatura' and payload > 50)
+
+  if (high_temperature):
+    publish.single(topic='ativar-led', payload='off', hostname=host)
+
+  else:
+    publish.single(topic='ativar-led', payload='on', hostname=host)
+
+  print(topic + ' = ' + str(payload))
 
 if __name__ == '__main__':
-  print('Listening...')
+  print('Escutando os tópicos "temperatura" e "umidade"...')
 
   client = mqtt.Client()
   client.on_message = on_message
 
   client.connect(host, port, 60)
 
-  client.subscribe('temperature')
-  client.subscribe('humidity')
+  client.subscribe('temperatura')
+  client.subscribe('umidade')
 
   while client.loop() == 0:
     pass
