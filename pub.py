c7f2c00805907747be7119177d940986d9772a44
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-07-06 15:38:33 -0300

    Update publisher topics

--- pub.py
+++ pub.py
@@ -5,5 +5,5 @@ host = 'test.mosquitto.org'
 port = 1883
 
 if __name__ == '__main__':
-  publish.single(topic='temperature', payload=36.5, hostname=host)
-  publish.single(topic='humidity', payload=25.7, hostname=host)
+  publish.single(topic='temperatura', payload=36.5, hostname=host)
+  publish.single(topic='umidade', payload=25.7, hostname=host)
